f34327a32e98ec490efe883a614a7e422a815730
keisuke sato <corydoras121@gmail.com>
2023-03-04 17:08:23 +0900

    [update]カラーパターン

--- src/js/shader/fragmentShader.glsl
+++ src/js/shader/fragmentShader.glsl
@@ -20,9 +20,18 @@ void main() {
   // gl_FragColor = vec4(vec3(uv, 1.0), 1.0);
 
 
+  // float distortion = vDistortion * 0.003;
+  // vec3 color = vec3(0.007, 0.313, 0.772);
+  // color = vec3(color.r + distortion, color.g + distortion, color.b + distortion);
+  // gl_FragColor = vec4(color, 1.0);
+
+
   float distortion = vDistortion * 0.003;
-  vec3 color = vec3(0.007, 0.313, 0.772);
-  color = vec3(color.r + distortion, color.g + distortion, color.b + distortion);
+  vec3 RED = vec3(0.831, 0.247, 0.552);
+  vec3 BLUE = vec3(0.007, 0.313, 0.772);
+  vec3 color1 = vec3(RED.r + distortion, RED.g + distortion, RED.b + distortion);
+  vec3 color2 = vec3(BLUE.r + distortion, BLUE.g + distortion, BLUE.b + distortion);
+  vec3 color = mix(color1, color2, sin(uTime * 0.2) * 0.5 + 0.5);
   gl_FragColor = vec4(color, 1.0);
 
 }
